2900929cb42ab005e360d8447cb893391e5deaa3
eaclumpkens <eaclumpkens@gmail.com>
2020-09-21 11:49:17 -0400

    style and hour functionality

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-
-// RENDER TODAY'S DATE
+// RENDER TODAY'S DATE 
 $("#date").text(`${moment().format('LL')}`);
 
-var currentTime = moment().format('LT');
-var oneDay = 24;
+// RENDER CURRENT TIME
+function currentTime() {
+    $("#current-time").text(`${moment().format('LTS')}`);
+}
 
-for (i = 0; i < oneDay; i++) {
-    var time;
+window.setInterval(currentTime, 1000);
 
-    if (i < 10) {
-        time = '0' + i + ':00';
-    } else {
-        time = i + ':00';
-    }
+// TIME VARIABLES
+
+var currentHour = moment().format('HH');
+var times = ["07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"];
+
+for (i = 0; i < times.length; i++) {
+    var hour = moment(times[i], 'HH:mm').format('LT');
+    var hourAbr = moment(times[i], 'HH:mm').format('HH');
 
     $("#time-slots").append(`
         <div class="row">
-            <h4 id="time" class="col-md-4">${time}</h4>
-            <input id="planner-input" type="text" class="col-md-4">
-            <i id="save-button" class="col-md-4 far fa-save"></i>
+            <h4 id="hour-${i}" class="time col-md-3">${hour}</h4>
+            <input id="input-${i}" data-hour="${hour}" type="text" class="planner-input col-md-6">
+            <i id="save-button" class="col-md-3 far fa-save"></i>
         </div>
     `)
+
+    if (currentHour === hourAbr) {
+        $(`#hour-${i}`).attr("style", "font-weight:400")
+        $(`#input-${i}`).attr("style", "background:#CCCCFF");
+    } else if (currentHour < hourAbr) {
+        // FUTURE HOURS
+        $(`#input-${i}`).attr("style", "background:#B5CCFF");
+    } else {
+        // PAST HOURS
+        $(`#input-${i}`).attr("style", "background:#87ccff");
+        $(`#input-${i}`).attr("disabled", "true");
+    }
+
 }
+
+console.log(currentHour)
